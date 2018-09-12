950f69c084615064725a76f16fd15c3d9c585c17
a <a>
2018-09-12 16:01:01 -0300

    correcao da classe produtoDao

--- src/main/java/br/com/db1/dao/ProdutoDao.java
+++ src/main/java/br/com/db1/dao/ProdutoDao.java
@@ -6,20 +6,19 @@ import javax.persistence.EntityManager;
 
 import br.com.db1.model.Produto;
 
-public class ProdutoDao extends DAO{
-	public void save(Produto produto){
+public class ProdutoDao extends DAO {
+	public void save(Produto produto) {
 		EntityManager manager = getEntityManager();
 
-		manager.getTransaction().begin();    
+		manager.getTransaction().begin();
 		manager.persist(produto);
-		manager.getTransaction().commit();  
+		manager.getTransaction().commit();
 	}
-	
-	public List<Produto> list(){
+
+	public List<Produto> list() {
 		EntityManager manager = getEntityManager();
 
 		return manager.createQuery("from Produto").getResultList();
 	}
-} 
 
 }
